31d4cf269ba481e8a624bb3b2095b3785df7ce0a
Pao Im <paoim@yahoo.com>
2019-11-28 12:42:29 -0600

    add draft on practice test 5

---
+++ MinnesotaPermitPracticeTest2019/js/app-practice-5.js
@@ -0,0 +1,574 @@
+const AppPracticeFive = (function() {
+	let q1 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q2 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q3 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q4 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q5 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q6 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q7 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q8 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q9 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q10 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q11 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q12 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q13 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q14 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q15 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q16 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q17 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q18 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q19 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q20 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q21 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q22 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q23 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q24 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q25 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q26 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q27 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q28 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q29 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q30 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q31 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q32 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q33 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q34 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q35 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q36 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q37 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q38 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q39 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		},
+		q40 = {
+		"type" : "text",
+		"orderedChoice" : 0,
+		"title" : "",
+		"src" : "sign_blank.png",
+		"choices" : [
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			{"q" : "", "a" : 0},
+			],
+		"hint" : "",
+		"result" : ""
+		};
+	let practice = [
+		q1, q2, q3, q4, q5, q6, q7, q8, q9, q10,
+		q11, q12, q13, q14, q15, q16, q17, q18, q19, q20,
+		q21, q22, q23, q24, q25, q26, q27, q28, q29, q30,
+		q31, q32, q33, q34, q35, q36, q37, q38, q39, q40
+	];
+	let getPractice = function() {
+		return practice;
+	};
+	return {
+		getPractice : getPractice
+	};
+})();
